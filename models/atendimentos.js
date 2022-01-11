b833f8c5c927fca8bdf24bb18f3efc45f725205d
rafavmaciel <rafavmaciel@yahoo.com.br>
2022-01-11 09:48:54 -0300

    validando campos e retornado erros

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -6,17 +6,45 @@ class Atendimento {
     adicionar(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
-
-        const sql = 'INSERT INTO atendimentos SET ?'
-
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-            if(erro) {
-                res.status(400).json(erro)
-            } else {
-                res.status(201).json(atendimento)
+        
+        // validações de dados 
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >= 5
+        // array com a descrição dos erros
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Cliente deve ter pelo menos cinco caracteres'
             }
-        })
+        ]
+        // filtra os campos que estão com o paramentro valido igual a false dentro da requisição 
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+        
+        if(existemErros) {
+            res.status(400).json(erros)
+        } else {
+            
+            const atendimentoDatado = {...atendimento, dataCriacao, data}
+    
+            const sql = 'INSERT INTO atendimentos SET ?'
+    
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro) {
+                    res.status(400).json(erro)
+                } else {
+                    res.status(201).json(atendimento)
+                }
+            })
+
+        }
+
     }
 }
 
